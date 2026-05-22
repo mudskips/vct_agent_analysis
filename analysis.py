219c9c21af05e68b473e69de5cd1bb212a5d6655
Jack Grady <275314523+mudskips@users.noreply.github.com>
2026-05-22 11:07:44 -0400

    create scatterplot with labels

--- analysis.py
+++ analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 #bringing in my csv files
 data_picks = pd.read_csv("vct_2026/agents/agents_pick_rates.csv")
@@ -12,13 +14,12 @@ filtered_data_picks["Pick Rate"] = filtered_data_picks["Pick Rate"].str.replace(
 
 #group by agent name and average the pick rates from each event
 grouped_by_agent_picks = filtered_data_picks.groupby("Agent")
-overall_pick_rates = grouped_by_agent_picks["Pick Rate"].mean()
+overall_pick_rates = grouped_by_agent_picks["Pick Rate"].mean().reset_index()
 
 filtered_data_wins = data_wins[(data_wins["Stage"] == "All Stages") & (data_wins["Match Type"] == "All Match Types")]
 
 grouped_by_agent_wins = filtered_data_wins.groupby("Agent")
 overall_wins = grouped_by_agent_wins[["Total Wins By Map", "Total Maps Played"]].sum()
-print(overall_wins)
 
 #add win rate column as the first column
 overall_wins.insert(0, "Win Rate", (overall_wins["Total Wins By Map"]/overall_wins["Total Maps Played"])*100)
@@ -27,3 +28,14 @@ overall_wins.insert(0, "Win Rate", (overall_wins["Total Wins By Map"]/overall_wi
 temp_table = pd.merge(overall_pick_rates, overall_wins, on="Agent")
 final_table = temp_table.drop(columns = ["Total Wins By Map", "Total Maps Played"])
 
+#start working on visual part
+plt.scatter(final_table["Pick Rate"], final_table["Win Rate"])
+plt.xlabel("Agent Pick Rate")
+plt.ylabel("Agent Win Rate")
+
+#add labels for agent names
+for i, row in final_table.iterrows():
+    plt.text(row["Pick Rate"], row["Win Rate"], row["Agent"])
+
+#display plot
+plt.show()
