96f1affdab683a99d0550e2fcbe9bda1693c1edd
Jack Grady <275314523+mudskips@users.noreply.github.com>
2026-05-22 08:27:48 -0400

    clean data and create general table

---
+++ analysis.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+#bringing in my csv files
+data_picks = pd.read_csv("vct_2026/agents/agents_pick_rates.csv")
+data_wins = pd.read_csv("vct_2026/agents/teams_picked_agents.csv")
+
+#sort data_picks to only include rows for all maps and all stages
+filtered_data_picks = data_picks[(data_picks["Map"] == "All Maps") & (data_picks["Stage"] == "All Stages")]
+
+#get rid of the % on the pick rates and turn it into a float
+filtered_data_picks["Pick Rate"] = filtered_data_picks["Pick Rate"].str.replace("%", "").astype(float)
+
+#group by agent name and average the pick rates from each event
+grouped_by_agent_picks = filtered_data_picks.groupby("Agent")
+overall_pick_rates = grouped_by_agent_picks["Pick Rate"].mean()
+
+filtered_data_wins = data_wins[(data_wins["Stage"] == "All Stages") & (data_wins["Match Type"] == "All Match Types")]
+
+grouped_by_agent_wins = filtered_data_wins.groupby("Agent")
+overall_wins = grouped_by_agent_wins[["Total Wins By Map", "Total Maps Played"]].sum()
+print(overall_wins)
+
+#add win rate column as the first column
+overall_wins.insert(0, "Win Rate", (overall_wins["Total Wins By Map"]/overall_wins["Total Maps Played"])*100)
+
+#combine these two dataframes into one readable one
+final_table = pd.merge(overall_pick_rates, overall_wins, on="Agent")
+
+
